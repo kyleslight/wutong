106f7e5d1fa6a822fcd9ddb7f5866bd079ad58da
kyleslight <likelakes07@gmail.com>
2013-12-08 19:17:44 +0800

    side comment change

--- static/js/opus.js
+++ static/js/opus.js
@@ -2,10 +2,10 @@ var isloadbox=false;
 var isregisterbox=false;
 var _showwel_flag = true;
 var elapseTime = 5000;
-var lastTimeOffsetHeight=0;
+var topOfSight=100;
 
 $(document).ready(function(){
-    initSideComment();
+    init();
     $.getJSON("/u/info", function (data) {
         var username;
         console.log(data);
@@ -46,22 +46,31 @@ $(document).ready(function(){
 
         // set height of side comment
         $(".opusSideComment").css({"height":(visibleHeght()+"px")});
-        for (var i =0; i<$(".opusMain").children().size(); i++) {
-            var offsetHeightOfOpusMainChild=$(".opusMain").children().eq(i).offset().top;
-            var heightOfOpusMainChild=$(".opusMain").children().eq(i).height();
-            if(
-               ( (offsetHeightOfOpusMainChild>=(top+200)&&offsetHeightOfOpusMainChild<(top+220)) )||
-               ( (offsetHeightOfOpusMainChild<=(top+200)&&((offsetHeightOfOpusMainChild+heightOfOpusMainChild))>(top+220) )) ){
-                $(".opusSideComment").stop();
-                $(".opusMain").children().css({"color":"rgb(68,68,68)","opacity":"1.0"});
-                $(".opusMain").children().eq(i).css({"color":"#680","opacity":"0.7"});
-                var heightOfSideCommnetChild=$(".opusSideCommentList"+i).eq(0).position().top + $(".opusSideComment").scrollTop();
-                $(".opusSideComment").children().css({"color":"black"});
-                $(".opusSideComment").children((".opusSideCommentList"+i)).css({"color":"#680"});
-                console.log(i);
-                $(".opusSideComment").animate({scrollTop:heightOfSideCommnetChild});
-            }
-        };
+
+        // side comment scroll with the sight of user 
+        if ($(".opusSideCommentWrap").css("display")!="none") {
+            for (var i =0; i<$(".opusMain").children().size(); i++) {
+                var offsetHeightOfOpusMainChild=$(".opusMain").children().eq(i).offset().top;
+                var heightOfOpusMainChild=$(".opusMain").children().eq(i).height();
+                if(
+                (
+                   ( (offsetHeightOfOpusMainChild>=(top+topOfSight)&&offsetHeightOfOpusMainChild<(top+topOfSight+20)) )||
+                   ( (offsetHeightOfOpusMainChild<=(top+topOfSight)&&((offsetHeightOfOpusMainChild+heightOfOpusMainChild))>(top+topOfSight+20) )) 
+                )&&($(".opusSideCommentList"+i).css("display")!="none")
+                   ){
+                    $(".opusSideComment").stop();
+                    $(".opusMain").children().css({"color":"rgb(68,68,68)","opacity":"1.0"});
+                    $(".opusMain").children().eq(i).css({"color":"#680","opacity":"0.7"});
+                    var heightOfSideCommnetChild=$(".opusSideCommentList"+i).eq(0).position().top + $(".opusSideComment").scrollTop();
+                    $(".activeOpusSideCommentList").removeClass("activeOpusSideCommentList");
+                    $(".activeOpusSideCommentNav").removeClass("activeOpusSideCommentNav");
+                    $(".opusSideComment").children((".opusSideCommentList"+i)).addClass("activeOpusSideCommentList");
+                    $(".opusSideCommentList"+i).eq(0).removeClass("activeOpusSideCommentList").addClass("activeOpusSideCommentNav");
+                    $(".opusSideComment").animate({scrollTop:heightOfSideCommnetChild},1000);
+                }
+            };
+        };  
+        $(".nullOpusSideCommentNav").css("display","none");
         return false;
     })
 
@@ -85,44 +94,73 @@ $(document).ready(function(){
 
     $("#expandSideComment").click(function(){
         if ($(".opusSideCommentWrap").css("display")=="none") {
-            $(".readMain").addClass("floatReadMain");
-            $(".opusSideCommentWrap").fadeIn();
-            $(".read").css({"width":"100%"});
-            var widthOfSideComment=($(window).width()-$(".floatReadMain").width()-42)+"px";
-            $(".opusSideComment").css({"height":(visibleHeght()+"px"),"width":widthOfSideComment});
+            expandSideComment();
         }else{
-            $(".readMain").removeClass("floatReadMain");
-            $(".opusSideCommentWrap").fadeOut();
-            $(".read").css({"width":"1060px"});
+            foldSideComment();
         }
     })
 
-    $(".paraNum").click(function(){
-        var indexOfPara=$(".paraNum").index($(this)).toString();
-        var offsetHeightOfSideCommnetChild=$(".opusSideCommentList"+indexOfPara).eq(0).position().top + $(".opusSideComment").scrollTop();
-        $(".opusSideComment").animate({scrollTop:+offsetHeightOfSideCommnetChild});
-        $(".opusSideComment").children().css({"color":"black"});
-        $(".opusSideComment").children((".opusSideCommentList"+indexOfPara)).css({"color":"#680"});
+    $(".opusMain").children().mouseover(function(){
+        if ($(".opusSideCommentWrap").css("display")!="none"){
+            $(this).children(".sideCommentView,.sideCommentEdit").fadeIn(50);
+        }
+    });
+    $(".opusMain").children().mouseleave(function(){
+        if ($(".opusSideCommentWrap").css("display")!="none"){
+            $(this).children(".sideCommentView,.sideCommentEdit").fadeOut(50);
+        }
+    })
+
+    $(".sideCommentView").click(function(){
+        $(".nullOpusSideCommentNav").css("display","none");
+        var indexOfPara=$(".sideCommentView").index($(this)).toString();
+        $(".activeOpusSideCommentList").removeClass("activeOpusSideCommentList");
+        $(".activeOpusSideCommentNav").removeClass("activeOpusSideCommentNav");
+        if ($(".opusSideCommentList"+indexOfPara).css("display")!="none") {
+            var offsetHeightOfSideCommnetChild=$(".opusSideCommentList"+indexOfPara).eq(0).position().top + $(".opusSideComment").scrollTop();
+            $(".opusSideComment").animate({scrollTop:+offsetHeightOfSideCommnetChild});
+            $(".opusSideComment").children((".opusSideCommentList"+indexOfPara)).addClass("activeOpusSideCommentList");
+            $(".opusSideCommentList"+indexOfPara).eq(0).removeClass("activeOpusSideCommentList").addClass("activeOpusSideCommentNav");
+        }else{
+            $(".opusSideCommentList"+indexOfPara).eq(0).fadeIn(500,function(){
+                var offsetHeightOfSideCommnetChild=$(".opusSideCommentList"+indexOfPara).eq(0).position().top + $(".opusSideComment").scrollTop();
+                $(".opusSideComment").animate({scrollTop:+offsetHeightOfSideCommnetChild});       
+            });     
+        }
         return false;
     })
 
 });
 
-function initSideComment(){
+function init(){
     for(var i=0;i<25;i++){
-        for(var j=0;j<5;j++){
+        var numOfComment=Math.floor(3*Math.random());
+        var appNav='<li class="opusSideCommentList opusSideCommentList'+i+' opusSideCommentNav" >'
+                    +'第'+i+'段评论('+'<span class="numOfParaComent">'+numOfComment+'</span>'+')'
+                    +'</li>';
+        var appNull='<li class="opusSideCommentList opusSideCommentList'+i+' opusSideCommentNav nullOpusSideCommentNav" >'
+                    +'这个段落目前还没有评论，你可以通过“编辑评论”开始创建'
+                    +'</li>';
+        if (numOfComment!=0) {
+            $(".opusSideComment").append(appNav);
+        }else{
+            $(".opusSideComment").append(appNull);
+        }
+        for(var j=0;j<numOfComment;j++){
             var appText='<li class="opusSideCommentList opusSideCommentList'+i+'" >'
-                    +   '第'+i+'段评论<br/>'
-                    +   'lalallalal<br/>'
-                    +   'lalallallalla<br/>'
-                    +   'lalallal<br/>'
-                    +   'lalallasdjfhjsdhf'
+                    +   '<a href="#" class="opusSideCommentListUserName">kyleslight</a>'
+                    +   '<div class="opusSideCommentContent">'
+                    +       'lalallalal<br/>'
+                    +       'lalallallalla<br/>'
+                    +       'lalallal<br/>'
+                    +       'lalallasdjfhjsdhf'
+                    +   '</div>'
                     +'</li>';
             $(".opusSideComment").append(appText);
         }
     }
     for (var i =0; i<$(".opusMain").children().size(); i++) {
-        var viewCommentButton='<a href="#" class="paraNum">'+i+'</a>';
+        var viewCommentButton='<a href="#" class="sideCommentView">查看评论</a><a href="#" class="sideCommentEdit">编辑评论</a>';
         $(".opusMain").children().eq(i).append(viewCommentButton);
     }
 }
@@ -150,4 +188,25 @@ function visibleHeght(){
        return $(window).height()-(topPartHeight()+buttomPartHeight()); 
     };
     return $(window).height()-(topPartHeight()+buttomPartHeight())+40;
+}
+
+function expandSideComment(){
+    $(".opusSideCommentWrap").removeClass("noTransition");
+    $(".readMain").addClass("floatReadMain");
+    $(".read").css({"width":"100%"});
+    var widthOfSideComment=($(window).width()-$(".floatReadMain").width()-42);
+    $(".opusSideComment").css({"height":(visibleHeght()+"px"),"width":widthOfSideComment+"px"});
+    $(".opusSideCommentList").css({"width":(widthOfSideComment-40)+"px"});
+    $(".opusSideCommentWrap").fadeIn();
+    $("#expandSideComment").text("收起侧评");
+}
+
+function foldSideComment(){
+    $(".opusSideCommentWrap").addClass("noTransition");
+    $(".opusSideCommentWrap").fadeOut();
+    $(".readMain").removeClass("floatReadMain");
+    $(".read").css({"width":"1060px"});
+    $(".opusMain").children().css({"color":"rgb(68,68,68)","opacity":"1.0"});
+    $(".sideCommentView,.sideCommentEdit").hide();
+    $("#expandSideComment").text("展开侧评");
 }
