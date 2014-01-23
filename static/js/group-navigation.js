2c9bc9849a8f0581b4799ebebb463db1e1b283ef
kyleslight <likelakes07@gmail.com>
2014-01-23 14:52:34 +0800

    创建小组跳转

--- static/js/group-navigation.js
+++ static/js/group-navigation.js
@@ -9,7 +9,18 @@ $(document).ready(function(){
 		$(".groupDynamic").fadeIn();		
 	});
 	$("#createGroupSend").click(function(){
-		document.getElementById("createGroupItem").submit();
+		var url = location.pathname.slice(0,-9) + '/g/create';
+		$.post(url,{
+			'name':$("#createGroupName").val(),
+			'intro':$("#createGroupIntro").val(),
+			'tags':$("#creareGroupTag").val(),
+			'is_public':isCreateGroupPublic(),
+		},function(data){
+			showError("小组"+$("#createGroupName").val()+"已成功申请<br>5秒后将自动跳转到新创建的小组<br>若跳转失败，请点击以下链接<br><a href='/g/"+data+"'>"+$("#createGroupName").val()+"</a>",20000);
+			setTimeout(function(){
+				self.location='/g/'+data;
+			},5000);
+		});
 	});
 });
 
